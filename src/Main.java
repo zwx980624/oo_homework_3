7ed1503a15aa19615d00c8523838b85b63bc20e6
zwx <853845818@qq.com>
2019-03-17 20:13:22 +0800

    main exception

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import differ.polys.Poly;
 import differ.terms.Term;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,10 +9,10 @@ public class Main {
         try (Scanner in = new Scanner(System.in)) {
             String str = in.nextLine();
             Poly poly = new Poly(str);
-            System.out.println(poly);
+            //System.out.println(poly);
             Term term = poly.diff();
             System.out.println(term);
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException | NoSuchElementException e) {
             System.out.println("WRONG FORMAT!");
         }
     }
